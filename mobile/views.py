88294bcef6e04b8d3c24481ad6aa6fe7b75dfe1f
Homkin <48763830+Cimer17@users.noreply.github.com>
2023-10-15 21:28:43 +0300

    fix lamp

--- mobile/views.py
+++ mobile/views.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse
 from .models import *
 import requests
 import json
-from django.views.decorators.csrf import csrf_exempt
 
 
 def get_data():
@@ -58,29 +57,32 @@ device_ids = ['045ab865-070d-4a99-994f-78cb17de2abd',
               'e4fd0567-d1d7-4064-9597-df391234cbac']
 
 
-def TurnOnOffLamp(token, device_id, turn):
+def TurnOnOffLamp(token: str, device_id: str, turn: bool):
     url = r'https://api.iot.yandex.net/v1.0/devices/actions'
     headers = {'Authorization': 'Bearer' + ' ' + token}
-    data = {'devices': [{'id': device_id, 'actions': [{'type': 'devices.capabilities.on_off', 'state': {
-        'instance': 'on', 'value': turn}} for device_id in device_ids]}]}
+    data = {'devices': [{'id': device_id, 'actions': [
+        {'type':  'devices.capabilities.on_off', 'state': {'instance':  'on', 'value': turn}}]}]}
     return requests.post(url=url, headers=headers, data=json.dumps(data)).content
 
 
 def lamp(turn):
-    for device_id in device_ids:
-        TurnOnOffLamp(token, device_id, turn)
+    if turn:
+        for i in range(len(device_ids)):
+            TurnOnOffLamp(token, device_ids[i], True)
+    else:
+        for i in range(len(device_ids)):
+            TurnOnOffLamp(token, device_ids[i], False)
 
 
-@csrf_exempt
 def toggle_lamp(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         isChecked = data.get("isChecked")
         if isChecked:
-            lamp(False)
+            lamp(True)
             message = "Лампочка включена"
         else:
-            lamp(True)
+            lamp(False)
             message = "Лампочка выключена"
         return JsonResponse({"message": message})
-    return JsonResponse({"error": "Invalid request method"}, status=400)  # Вернуть JSON-ответ для GET-запроса
+    return JsonResponse({"error": "Invalid request method"}, status=400)
